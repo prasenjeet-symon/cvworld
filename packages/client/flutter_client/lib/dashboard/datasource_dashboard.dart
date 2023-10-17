7fbf09766de48204b1022c0d526b0b27af9d95f1
Pawan Kumar <iampawanbhatia@gmail.com>
2023-10-18 00:05:34 +0530

    changed domain name

--- packages/client/flutter_client/lib/dashboard/datasource_dashboard.dart
+++ packages/client/flutter_client/lib/dashboard/datasource_dashboard.dart
@@ -751,7 +751,7 @@ class DashboardDataService {
   late Uri adminDetailsRoute;
 
   DashboardDataService() {
-    origin = 'https://www.cvworld.me';
+    origin = 'https://cvworld.me';
     // origin = 'http://localhost:8080';
     signInRoute = Uri.parse('$origin/server/auth/sign_in_as_admin');
     resetPasswordRoute = Uri.parse('$origin/server/api_admin/reset_password');
