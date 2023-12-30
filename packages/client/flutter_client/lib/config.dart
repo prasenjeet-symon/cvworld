e3e8bcd79059461add953de3c781d62e6a51d28e
Pawan Kumar <iampawanbhatia@gmail.com>
2023-12-30 18:48:27 +0100

    Added my keys, SHA-1

--- packages/client/flutter_client/lib/config.dart
+++ packages/client/flutter_client/lib/config.dart
@@ -1,9 +1,7 @@
 class ApplicationConfiguration {
-  static const String GOOGLE_WEB_CLIENT_ID = "599994784077-8b5i8jsi9s75ddqv8p8v1031gk2mise7.apps.googleusercontent.com";
+  static const String GOOGLE_WEB_CLIENT_ID = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
   static const bool IS_LOCAL = false;
   static const bool IS_SERVERLESS = true;
-  // https://www.cvworld.me
   // http://localhost:8081
-  // https://native-humorous-mule.ngrok-free.app
   static const String API_URL = "https://www.cvworld.me";
 }
