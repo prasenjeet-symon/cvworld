98eb9b5fa81380193fd0185ab6de8fcb021c21ce
prasenjeet-symon <prasenjeetsymon@outlook.com>
2024-02-12 11:43:45 +0530

    fixed delete account bugs

--- packages/client/flutter_client/lib/config.dart
+++ packages/client/flutter_client/lib/config.dart
@@ -1,11 +1,11 @@
 class ApplicationConfiguration {
   static const googleWebClientId = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
   static const googleAndroidClientId = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
-  //static const isLocal = false;
-  static const isLocal = true;
+  static const isLocal = false;
+  // static const isLocal = true;
   static const isServerLess = true;
   static const String appName = 'CV World';
   static const String appVersion = '1.0';
-  static const String apiUrl = "http://localhost:8081";
-  //static const String apiUrl = "https://www.cvworld.me";
+  // static const String apiUrl = "http://localhost:8081";
+  static const String apiUrl = "https://www.cvworld.me";
 }
