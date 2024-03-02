7abb06432ce7230cdfae983b0bf767d6334dbf59
prasenjeet-symon <prasenjeetsymon@outlook.com>
2024-03-02 08:11:24 +0530

    building

--- packages/client/flutter_client/lib/config.dart
+++ packages/client/flutter_client/lib/config.dart
@@ -11,17 +11,19 @@ class ApplicationConfiguration {
   static const int subscriptionWaitingTime = 250; // sec
 
   // For production
-  // static const googleWebClientId = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
-  // static const googleWebClientId = "963211799164-0utlh7pi95kfvc8rghrv3o71rge9jvbc.apps.googleusercontent.com";
-  // static const isLocal = false;
-  // static const String apiUrl = "https://www.cvworld.me";
+  
+  static const googleWebClientId = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
+  static const googleWebClientId = "963211799164-0utlh7pi95kfvc8rghrv3o71rge9jvbc.apps.googleusercontent.com";
+  static const isLocal = false;
+  static const String apiUrl = "https://www.cvworld.me";
 
   ///
   ///
   ///
   /// For local
-  static const googleAndroidClientId = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
-  static const googleWebClientId = "963211799164-0utlh7pi95kfvc8rghrv3o71rge9jvbc.apps.googleusercontent.com";
-  static const isLocal = true;
-  static const String apiUrl = "http://localhost:8081";
+  
+  // static const googleAndroidClientId = "526173453078-5bt1icrr45ub28erv39j62qkpnd5473m.apps.googleusercontent.com";
+  // static const googleWebClientId = "963211799164-0utlh7pi95kfvc8rghrv3o71rge9jvbc.apps.googleusercontent.com";
+  // static const isLocal = true;
+  // static const String apiUrl = "http://localhost:8081";
 }
